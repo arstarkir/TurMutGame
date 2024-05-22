306a4fae0ce262aa3508c4bbf6ad07a9440242c5
arstarkir <arstarkir@gmail.com>
2024-05-21 22:01:34 -0400

    Cammera controler and area select

--- TurMutGame/Assets/Scripts/Controls/Camera Controls.cs
+++ TurMutGame/Assets/Scripts/Controls/Camera Controls.cs
@@ -7,6 +7,10 @@ public class CameraControls : MonoBehaviour
     [SerializeField] float speed = 1;
     [SerializeField] float scrollStrength = 1;
 
+    [SerializeField] Material notActiveTrigger;
+    [SerializeField] Material activeTrigger;
+
+    [HideInInspector] public GameObject active;
     void OnGUI()
     {
         transform.position -= Vector3.up * scrollStrength * Input.GetAxis("Mouse ScrollWheel");
@@ -19,7 +23,30 @@ public class CameraControls : MonoBehaviour
 
         mousePos.x -= Screen.width / 2;
         mousePos.z -= Screen.height / 2;
+        if ((mousePos.x * 0.01f * speed) * (mousePos.x * 0.01f * speed) > 2 ||
+            (mousePos.z * 0.01f * speed) * (mousePos.z * 0.01f * speed) > 2)
+            transform.position -= mousePos * 0.01f * speed * Time.deltaTime;
 
-        transform.position -= mousePos*0.01f*speed*Time.deltaTime;
+        Ray ray;
+        RaycastHit hit1;
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit1))
+        {
+
+            if (hit1.collider.gameObject.layer == 13)
+            {
+                if (active != hit1.collider.gameObject)
+                {
+                    if (active)
+                    {
+                        active.GetComponent<Renderer>().material = notActiveTrigger;
+                    }
+                    active = hit1.collider.gameObject;
+                }
+                active.GetComponent<Renderer>().material = activeTrigger;
+            }
+            else
+                active.GetComponent<Renderer>().material = notActiveTrigger;
+        }
     }
 }
